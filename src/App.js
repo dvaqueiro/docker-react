adff946b0420968f89d65789e384ed75401edaae
dvaqueiro <dvaqueiro@boardfy.com>
2019-06-11 20:31:24 +0200

    small update to trigger travis buildwq

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there from docker-compose!
+          Hi there!
         </p>
         <a
           className="App-link"
